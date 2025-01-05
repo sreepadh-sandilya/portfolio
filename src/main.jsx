b05dec86eb9aff3e0c4d57d3f45536e44871f2e4
sreepadh-sandilya <sreepadhkadapa@gmail.com>
2025-01-05 13:30:42 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { HashRouter as Router } from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom'; // Use BrowserRouter or HashRouter, not both.
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/portfolio"> {/* Add basename here */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
-)
+);
